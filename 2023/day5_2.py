57c5737f7ac77246d5d207f7c6855be7903c8554
rost <j.rost@tu-braunschweig.de>
2023-12-07 18:20:35 +0100

    solution 5_2

--- 2023/day5_2.py
+++ 2023/day5_2.py
@@ -1,6 +1,8 @@
 import time
 from tqdm import tqdm
 import numpy as np
+
+
 start_time = time.time()
 with open("2023/inputs/day5_1.txt") as file:
     data=file.readlines()
@@ -12,10 +14,7 @@ seeds_setup=[int(x) for x in seeds]
 seeds=[]
 for i, entry in tqdm(enumerate(seeds_setup)):
     if i%2!=0:
-        seeds.append([seeds_setup[i-1], seeds_setup[i]])
-
-print(seeds)
-
+        seeds.append([seeds_setup[i-1], seeds_setup[i-1]+seeds_setup[i]])
 
 mapping={}
 for i,line in tqdm(enumerate(data)):
@@ -37,25 +36,17 @@ for i,line in tqdm(enumerate(data)):
 print("parsing finished")
 
 def split_interval(seed_start, seed_end, point1, point2,destination):
-
     before = [seed_start, min(seed_end,point1)]
     between = [max(seed_start, point1), min(point2, seed_end)]
     after = [max(point2, seed_start), seed_end]
-
-    if before[0]>before[1]:
+    if before[0]>=before[1]:
         before=[None, None]
-    else:
-        before= [before[0], before[1]-before[0]]
-    if between[0]>between[1]:
+    if between[0]>=between[1]:
         between=[None, None]
-    else:
-        #between=[between[0]-point1 +destination, 
-        #         between[1]-point1+destination]
-        between= [between[0], between[1]-between[0]]
-    if after[0]>after[1]:
+    if after[0]>=after[1]:
         after=[None, None]
     else:
-        after= [after[0], after[1]-after[0]]
+        pass
     return before, between, after
 
 def pass_through(input_map, map):
@@ -63,23 +54,16 @@ def pass_through(input_map, map):
     passed=[]
     for entry in map.values(): 
         not_passed=[]
-        #print(seeds)
         for x in seeds:
-            seed_start=x[0]
-            seed_end= x[0]+x[1]
-            before, between, after = split_interval(seed_start, seed_end,
+            before, between, after = split_interval(x[0], x[1],
                                                     entry["source"], entry["source"]+ entry["interval"], entry["destination"])
-            
             if between[0] != None:
-                between[0]+= entry["destination"]-entry["source"]
-                #between[1] -=-entry["source"]+entry["destination"]
+                between=[between[0]-entry["source"]+entry["destination"], between[1]-entry["source"]+entry["destination"]]
                 passed.append(between)
-            
             if before[0]!=None:
                 not_passed.append(before)
             if after[0]!=None:
                 not_passed.append(after)
-            #exit()
         seeds = not_passed
     return passed + not_passed
         
@@ -88,14 +72,10 @@ def pass_through(input_map, map):
 input_map=seeds
 for i, map in enumerate(mapping.values()):
     input_map=pass_through(input_map, map)
-    print(f"{i},  {input_map}")
-    #exit()
 
-min1=100000000000
-for i in input_map:
-    if i[0]<min1:
-        min1=i[0]
-print("result ",min1)
+starts= [i[0] for i in input_map]
+
+print("result ",min(starts))
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
